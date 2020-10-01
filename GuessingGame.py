4b80471494a04a086169d05b30ee30b4a4a46dad
Aarni Honka <4585172+shopro@users.noreply.github.com>
2020-10-01 06:29:17 +0300

    Added some functionality and f-strings

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,12 +1,26 @@
-highest=10
-answer=7
-guess=input("Guess a number from 0 to %d: " %highest)
-while(int(guess)!=answer):
-    if(int(guess)<answer):
-        print("Answer is higher")
-    else:
-        print("Answer is lower")
-    guess=input("Guess a number from 0 to %d: " %highest)
-input("You are a winner!")
+import random
+
+lowest = 1
+highest = 25
+
+# Select random number
+answer = random.randint(lowest, highest)
+
+def guess():
+    guess=input(f"Guess a number from {lowest} to {highest}: ")
+    
+    try_number = 0
+    
+    while(int(guess)!=answer):
+        try_number += 1
+        if(int(guess)<answer):
+            print("Answer is higher")
+        else:
+            print("Answer is lower")
+        guess=input(f"Guess a number from {lowest} to {highest}: ")
+        
+    print(f"You are a winner! It took you {try_number} try(s)")
     
             
+if __name__ == "__main__":
+    guess()
